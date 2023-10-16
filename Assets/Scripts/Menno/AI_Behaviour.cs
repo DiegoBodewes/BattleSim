c19554540a64bd684495e29c77b6435a7fd273fe
Menno <114731270+Buldz@users.noreply.github.com>
2023-10-16 14:06:58 +0200

    ai behaviour script documentatie

---
+++ Assets/Scripts/Menno/AI_Behaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Behaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //enemies herkennen
+
+        //loopt richting enemy
+
+        //ability
+
+        //health = 0 destroy
+    }
+}
