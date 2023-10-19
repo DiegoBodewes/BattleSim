85887b7ea08e9c7ac1f934b9aa628cc88bfa1353
Menno <114731270+Buldz@users.noreply.github.com>
2023-10-19 14:28:36 +0200

    ShadowWizard, StoneCaster scripts + prefabs

--- Assets/Scripts/Menno/Projectile.cs
+++ Assets/Scripts/Menno/Projectile.cs
@@ -1,25 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class Projectile : MonoBehaviour
 {
-    public float destroyTime = 10f;
-    public int damage = 12;
+    public float destroyTime = 10f; //projectile destroy time
+    public int minDamage = 15;
+    public int MaxDamage = 18;
+
+    public float speed = 5f; //projectile speed
+    Vector3 _direction; //the direction of the projectile
+    bool isReady; //to know when the projectile direction is set
+
+    void Awake()
+    {
+        isReady = false;
+    }
+
+    public void SetDirection(Vector3 direction)
+    {
+        //set the direction normalized, to get an unit vector
+        _direction = direction.normalized;
+
+        isReady = true; //set flag to true
+    }
+
+    void Update()
+    {
+        if (isReady)
+        {
+            //get the bullet's current position
+            Vector3 position = this.transform.position;
+
+            //compute the bullet's new position
+            position += _direction * speed * Time.deltaTime;
+
+            //update the bullet's position
+            this.transform.position = position;
+        }
+
+        Destroy(gameObject, destroyTime);
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Wizard wizard = collision.collider.GetComponent<Wizard>();
+        int damage = Random.Range(minDamage, MaxDamage); //projectile damage
+
+        ShadowWizard wizard = collision.collider.GetComponent<ShadowWizard>();
         if (wizard != null)
         {
             wizard.TakeDamage(damage);
         }
+        
+        if(collision.gameObject.tag == "Team1") Destroy(this.gameObject);
+        if (collision.gameObject.tag == "Team2") Destroy(this.gameObject);
 
-        Destroy(this.gameObject);
-    }
-
-    private void Update()
-    {
-        Destroy(gameObject, destroyTime);
     }
 }
+
--- Assets/Scripts/Menno/StoneCaster.cs
+++ Assets/Scripts/Menno/StoneCaster.cs
@@ -3,33 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class ShadowWizard : MonoBehaviour
+public class StoneCaster : MonoBehaviour
 {
     public bool isTeam1 = true;
 
     public NavMeshAgent agent;
 
     public Transform enemy;
+    public Transform ally;
 
     public LayerMask whatIsGround;
     public LayerMask WhatIsEnemy;
+    public LayerMask WhatIsAlly;
 
     [Header("Options")]
     public float health = 50;
+
+    public float enemySightRange;
     public float timeBetweenAttack;
-    public float sightRange;
     public float attackRange;
 
-    //Attacking
-    [Header("Attacking")]
+    public float shieldRange;
+    public float timeBetweenShield;
+
+    //Abilities
+    [Header("Abilities")]
     bool alreadyAttacked;
+    bool alreadyShield;
+
     public GameObject ProjectilePrefab;
+    public GameObject ShieldPrefab;
+
     public Transform firePoint;
 
     //States
     [Header("States")]
     public bool EnemyInSightRange;
     public bool EnemyInAttackRange;
+    public bool AllyInShieldRange;
 
     // Start is called before the first frame update
     void Start()
@@ -48,21 +59,26 @@ public class ShadowWizard : MonoBehaviour
 
     private void Update()
     {
-        if(isTeam1 == true)
+        if (isTeam1 == true)
         {
             enemy = GameObject.FindWithTag("Team2").transform;
+            ally = GameObject.FindWithTag("Team1").transform;
         }
         else if (isTeam1 == false)
         {
             enemy = GameObject.FindWithTag("Team1").transform;
+            ally = GameObject.FindWithTag("Team2").transform;
         }
 
         //Check for sight and attack range
-        EnemyInSightRange = Physics.CheckSphere(transform.position, sightRange, WhatIsEnemy);
+        EnemyInSightRange = Physics.CheckSphere(transform.position, enemySightRange, WhatIsEnemy);
         EnemyInAttackRange = Physics.CheckSphere(transform.position, attackRange, WhatIsEnemy);
+
+        AllyInShieldRange = Physics.CheckSphere(transform.position, shieldRange, WhatIsAlly);
 
         if (EnemyInSightRange && !EnemyInAttackRange) Chase();
         if (EnemyInSightRange && EnemyInAttackRange) Attack();
+        if (AllyInShieldRange) Attack();
     }
 
     private void Chase()
@@ -80,7 +96,15 @@ public class ShadowWizard : MonoBehaviour
         if (!alreadyAttacked)
         {
             //Ability
-            Projectile();
+
+            //instantiate an projectile
+            GameObject projectile = (GameObject)Instantiate(ProjectilePrefab, firePoint.transform.position, Quaternion.identity);
+
+            //compute the projectile direction towards the enemy
+            Vector3 direction = enemy.transform.position - projectile.transform.position;
+
+            //Set the projectile direction
+            projectile.GetComponent<Projectile>().SetDirection(direction);
 
             //
 
@@ -89,9 +113,33 @@ public class ShadowWizard : MonoBehaviour
         }
     }
 
+    private void Shield()
+    {
+        //Make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(ally);
+
+        if (!alreadyShield)
+        {
+            //Ability
+            
+
+            //
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetShield), timeBetweenShield);
+        }
+    }
+
     private void ResetAttack()
     {
         alreadyAttacked = false;
+    }
+
+    private void ResetShield()
+    {
+        alreadyShield = false;
     }
 
     public void TakeDamage(int damage)
@@ -104,25 +152,15 @@ public class ShadowWizard : MonoBehaviour
         }
     }
 
-    void Projectile()
-    {
-        {
-                //instantiate an projectile
-                GameObject projectile = (GameObject)Instantiate(ProjectilePrefab, firePoint.transform.position, Quaternion.identity);
-
-                //compute the projectile direction towards the enemy
-                Vector3 direction = enemy.transform.position - projectile.transform.position;
-
-                //Set the projectile direction
-                projectile.GetComponent<Projectile>().SetDirection(direction);
-        }
-    }
+  
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, attackRange);
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, sightRange);
+        Gizmos.DrawWireSphere(transform.position, enemySightRange);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, shieldRange);
     }
 }
