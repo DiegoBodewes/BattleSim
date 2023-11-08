f4d8d4a13c43fc668e79b9d1c846c394118dd26a
Menno <114731270+Buldz@users.noreply.github.com>
2023-11-08 15:34:28 +0100

    pause menu

--- Assets/Scripts/Menno/PauseMenu.cs
+++ Assets/Scripts/Menno/PauseMenu.cs
@@ -18,13 +18,11 @@ public class PauseMenu : MonoBehaviour
     {
         PauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
-        Cursor.lockState = CursorLockMode.Confined;
     }
 
     public void Play()
     {
         PauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
-        Cursor.lockState = CursorLockMode.Locked;
     }
 }
